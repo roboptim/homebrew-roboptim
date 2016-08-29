afc5b33bb8d13a6b2c6f51d8f257fbb7e6fca7ec
Benjamin Chrétien <chretien.b@gmail.com>
2016-08-29 19:58:43 +0200

    roboptim-capsule: update formula

--- roboptim-capsule.rb
+++ roboptim-capsule.rb
@@ -1,14 +1,18 @@
 require "formula"
 
 class RoboptimCapsule < Formula
-  homepage "http://www.roboptim.net/"
+  homepage "http://www.roboptim.net"
+  url "https://github.com/roboptim/roboptim-capsule/releases/download/v3.2/roboptim-capsule-3.2.tar.bz2"
+  sha256 "dd591bfa1f76db8ddcc22a33ea9811a6c4ef62beee8f7a8c6d72a987e6544150"
 
   head 'https://github.com/roboptim/roboptim-capsule.git'
 
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on "pkg-config" => :build
-  depends_on "qhull"
+  depends_on "homebrew/science/qhull"
+  # FIXME: explicit eigen dependency required for pkg-config
+  depends_on "eigen" => :build
   depends_on "roboptim/roboptim/roboptim-core"
 
   #FIXME: should be optional / configurable
@@ -20,15 +24,29 @@ class RoboptimCapsule < Formula
     # Always remove -Werror flag.
     args << "-DCXX_DISABLE_WERROR:BOOL=ON"
 
+    # Fixes:
+    # ------
+    #
+    # 1. Always link to liblog4cxx
+    #
+    # To be fixed upstream.
+    if not build.head?
+      liblog4cxx = %x[pkg-config --libs liblog4cxx].chomp
+      args << "-DCMAKE_EXE_LINKER_FLAGS='" + liblog4cxx + "'"
+      args << "-DCMAKE_MODULE_LINKER_FLAGS='" + liblog4cxx + "'"
+    end
+
     # Remove SOVERSION.
     inreplace 'src/CMakeLists.txt',
               'SET_TARGET_PROPERTIES(${LIBRARY_NAME}',
               '# SET_TARGET_PROPERTIES(${LIBRARY_NAME}'
 
-    ENV.append_path "PKG_CONFIG_PATH",
-                    "#{HOMEBREW_PREFIX}/lib/pkgconfig"
     system "cmake", ".", *args
     system "make", "install"
+
+    if build.head?
+      system "make", "test"
+    end
   end
 
   test do
